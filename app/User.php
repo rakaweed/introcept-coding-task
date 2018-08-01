f7a7bb3d652d4c45844c6db92d8439a735dd1c91
Deewakar Acharya <phoenix.sujan@gmail.com>
2018-08-01 03:53:55 +0000

    Apply fixes from StyleCI

--- app/User.php
+++ app/User.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-	protected $table = 'tbl_users';
+    protected $table = 'tbl_users';
     protected $fillable = [
-        'name', 'gender', 'phone', 'email', 'address', 'nationality', 'date_of_birth', 'education', 'preferred_mode_contact', 
+        'name', 'gender', 'phone', 'email', 'address', 'nationality', 'date_of_birth', 'education', 'preferred_mode_contact',
     ];
 }
